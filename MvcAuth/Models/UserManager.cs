cb90992f9ed47b13de13e5a5516cdfa1bd541e1d
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-02-13 21:01:59 +0200

    user manager

---
+++ MvcAuth/Models/UserManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcAuth.Models
+{
+    public class UserManager
+    {
+        private List<User> _users = new List<User>();
+
+        public UserManager()
+        {
+            // add users: bill / gates / admin,owner
+            // steve / ballmer / developers
+            // satya / nadella / ceo,admin
+            // share / point / developers
+        }
+
+        public User Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UserExists(string username)
+        {
+            return false;
+        }
+    }
+}
