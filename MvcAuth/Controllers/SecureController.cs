1e940912cfd1395606d464646212ade01313b792
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-02-13 22:03:22 +0200

    principal

---
+++ MvcAuth/Controllers/SecureController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAuth.Controllers
+{
+    public class SecureController : Controller
+    {
+        // GET: Secure
+        [Authorize(Roles="users,admin,developers,owner")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
